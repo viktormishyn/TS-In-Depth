cb35afde3c87c6b74ac4d5c275c8f9ebaf82a81e
viktormishyn <viktormishyn@gmail.com>
2022-04-12 11:28:05 +0300

    Tasks 03.01, 03.02, 03.03, 03.04

--- src/app.ts
+++ src/app.ts
@@ -56,16 +56,16 @@ function getAllBooks(): readonly Book[] {
     return books;
 }
 
-function logFistAvailable(books: readonly Book[]): void {
+function logFistAvailable(books: readonly Book[] = getAllBooks()): void {
     console.log(`Total number of books: ${books.length}`);
     // const title = books.find(book => book.available)?.title;
     const title = books.find(({ available }) => available)?.title;
     console.log(`First available book: ${title}`);
 }
 
-function getBookTitlesByCategory(category: Category): string[] {
+function getBookTitlesByCategory(category: Category = Category.JavaScript): string[] {
     const books = getAllBooks();
-    const titles = books.filter(book => book.category == category).map(book => book.title);
+    const titles = books.filter(book => book.category === category).map(book => book.title);
     return titles;
 }
 
@@ -100,3 +100,100 @@ function calcTotalPages(): void {
 // calcTotalPages();
 
 // ==================================================================
+
+// ========================== 03 Functions ==========================
+
+function createCustomerID(name: string, id: number): string {
+    return `${id}-${name}`;
+}
+
+const myID = createCustomerID('Ann', 10);
+
+// let idGenerator: (name: string, id: number) => string;
+let idGenerator: typeof createCustomerID;
+idGenerator = (name: string, id: number) => `${id}-${name}`;
+idGenerator = createCustomerID;
+
+function createCustomer(name: string, age?: number, city?: string): void {
+    console.log(`Name: ${name}`);
+    age ? console.log(`Age: ${age}`) : null;
+    city ? console.log(`City: ${city}`) : null;
+    console.log('\n');
+}
+
+// const getBookByID = (id: number): Book => {
+function getBookByID(id: Book['id']): Book {
+    return getAllBooks().find(book => book.id === id);
+}
+
+function сheckoutBooks(customer: string, ...bookIDs: Book['id'][]): string[] {
+    console.log(`Customer: ${customer}`);
+    const books = getAllBooks();
+    return books.filter(book => bookIDs.includes(book.id) && book.available).map(book => book.title);
+}
+
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(id: number, available: boolean): string[];
+// function getTitles(...args: [string | boolean] | [number, boolean]): string[] {
+function getTitles(...args: any[]): string[] {
+    const books = getAllBooks();
+
+    if (args.length === 1) {
+        const [arg] = args;
+
+        if (typeof arg === 'string') {
+            return books.filter(book => book.author === arg).map(book => book.title);
+        } else if (typeof arg === 'boolean') {
+            return books.filter(book => book.available === arg).map(book => book.title);
+        }
+    } else if (args.length === 2) {
+        const [id_arg, available_arg] = args;
+
+        if (typeof id_arg === 'number' && typeof available_arg === 'boolean') {
+            return books
+                .filter(({ id, available }) => id === id_arg && available === available_arg)
+                .map(book => book.title);
+        }
+    }
+    return [];
+}
+
+function assertStringValue(val: any): asserts val is string {
+    if (typeof val !== 'string') {
+        throw new Error('Value should have been a string');
+    }
+}
+
+function bookTitleTransform(title: any): string | never {
+    assertStringValue(title);
+    return [...title].reverse().join('');
+}
+
+// ==================================================================
+
+// console.log(myID);
+// console.log(idGenerator('Ann', 10));
+
+// createCustomer('Ann');
+// createCustomer('Ann', 75);
+// createCustomer('Ann', 75, 'London');
+
+// console.log(getBookTitlesByCategory());
+// logFistAvailable();
+
+// console.log(getBookByID(1));
+
+// const myBooks: string[] = сheckoutBooks('Ann', 1, 2, 4);
+// // const myBooks: string[] = сheckoutBooks('Ann', ...[1, 2, 4]);
+// console.log(myBooks);
+
+// console.log(getTitles(1, true));
+// console.log(getTitles('Lea Verou'));
+
+console.log(bookTitleTransform('12345'));
+console.log(bookTitleTransform(12356));
+
+// ==================================================================
+
+// ========================== 04 Interfaces =========================
